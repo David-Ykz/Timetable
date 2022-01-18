b71ac5cbaee71c021540deb1126034c36a3016aa
David-Ykz <nerdkid314159@gmail.com>
2022-01-18 09:13:15 -0500

    Now reads from updated course list

--- Course.java
+++ Course.java
@@ -1,31 +1,40 @@
 import java.util.ArrayList;
 
 class Course {
- private int courseId;
- private int teacherIds[]; //teachers who can teach this class
- private String code;
- private String name;
- private int grade;
- private String type;
- private int credits;
- private ArrayList<String> prerequisites;
- private ArrayList<String> corequisites;
- private String special;
- private int cap;
-
- Course(int courseId, String code, String name, int grade, String type, int credits, ArrayList<String> prerequisites, ArrayList<String> corequisites, String special, int cap) {
-     this.courseId = courseId;
-     this.code = code;
-     this.name = name;
-     this.grade = grade;
-     this.type = type;
-     this.credits = credits;
-     this.prerequisites = prerequisites;
-     this.corequisites = corequisites;
-     this.special = special;
-     this.cap = cap;
- }
-
+    private int courseId;
+    private int teacherIds[]; //teachers who can teach this class
+    private String code;
+    private String name;
+    private int grade;
+    private String type;
+    private double credits;
+    private ArrayList<String> prerequisites;
+    private ArrayList<String> corequisites;
+    private String special;
+    private int cap;
+    
+    Course(int courseId, String code, String name, int grade, String type, double credits, ArrayList<String> prerequisites, ArrayList<String> corequisites, String special, int cap) {
+        this.courseId = courseId;
+        this.code = code;
+        this.name = name;
+        this.grade = grade;
+        this.type = type;
+        this.credits = credits;
+        this.prerequisites = prerequisites;
+        this.corequisites = corequisites;
+        this.special = special;
+        this.cap = cap;
+    }
+    
+    Course(int courseId, String code, String name, double credits, String type, int cap) {
+        this.courseId = courseId;
+        this.code = code;
+        this.name = name;
+        this.credits = credits;
+        this.type = type;
+        this.cap = cap;
+    }
+ 
  public void printInfo() {
      System.out.println("Code: " + code);
      System.out.println("Name: " + name);
